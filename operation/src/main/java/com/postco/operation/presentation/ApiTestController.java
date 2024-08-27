f006170989cf138d7f20578c861d0e0677d607b4
Kim, Yerim <52775286+yerimee82@users.noreply.github.com>
2024-08-27 09:49:46 +0900

    Feat: 카프카 설정 완료 (#5)
    * Feat: kafka docker compose 설정 추가
    
    * Feat: 그라파나 및 프로메테우스 설정, core kafkaConfig 설정
    
    * Feat: Producer, Consumer Test 완료

--- operation/src/main/java/com/postco/operation/presentation/ApiTestController.java
+++ operation/src/main/java/com/postco/operation/presentation/ApiTestController.java
@@ -1,17 +1,30 @@
 package com.postco.operation.presentation;
 
 import com.postco.operation.presentation.entity.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.postco.operation.service.TestProducer;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class ApiTestController {
+    private final TestProducer producer;
     @GetMapping("/{id}")
     public User getUserById(@PathVariable long id) {
         return new User(id, "조업", "chandex@example.com");
+    }
+
+    @GetMapping
+    public String test() {
+        return "Kafka Test";
+    }
+
+    @PostMapping
+    public String sendMessage(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+        return message;
     }
 }
 
