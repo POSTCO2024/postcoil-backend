515dbf42c0a947f41413e1a663a02c4036d2f511
hanriverkitty <danieltls@naver.com>
2024-09-05 21:26:16 +0900

    Chore: 변수이름들 수정

--- control/src/main/java/com/postco/control/domain/RollUnit.java
+++ control/src/main/java/com/postco/control/domain/RollUnit.java
@@ -17,8 +17,8 @@ public class RollUnit implements com.postco.core.entity.Entity, Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "roll_name", nullable = false)
-    private String rollName;
+    @Column(name = "roll_unit_name", nullable = false)
+    private String rollUnitName;
 
     @Column(name = "criteria_value", nullable = false)
     private double criteriaValue;
