e3ea87fe4ece1fbe67398f05d2a4308b4c287e08
dudco0040 <dudco0040@gmail.com>
2024-09-08 23:47:45 +0900

    Feat: 에러재조회화면 API 연결을 위한 DTO 수정

--- control/src/main/java/com/postco/control/presentation/ControlController.java
+++ control/src/main/java/com/postco/control/presentation/ControlController.java
@@ -6,6 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 import com.postco.control.service.ManagementService;
 import com.postco.control.service.impl.CriteriaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
