c2a3eb7ac2e715056e4a701f36362a7f17fce583
dudco0040 <dudco0040@gmail.com>
2024-09-08 20:21:57 +0900

    Fix: CrossOrigin & Redis 에러 수정

--- control/src/main/java/com/postco/control/presentation/ControlController.java
+++ control/src/main/java/com/postco/control/presentation/ControlController.java
@@ -6,6 +6,7 @@ import com.postco.control.presentation.dto.response.MaterialDTO;
 import com.postco.control.presentation.dto.response.TargetMaterialDTO;
 import com.postco.control.service.ControlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/control")
+@CrossOrigin(origins = "http://localhost:4000")
 public class ControlController {
 
     private final ControlService controlService;
