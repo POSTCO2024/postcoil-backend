d2f5134e9eecbf58ba886076dd29769b5ff21fb2
hanriverkitty <danieltls@naver.com>
2024-09-12 00:29:24 +0900

    Rename: 변수이;름 변경

--- core/src/main/java/com/postco/core/dto/ApiResponseDTO.java
+++ core/src/main/java/com/postco/core/dto/ApiResponseDTO.java
@@ -1,16 +1,18 @@
 package com.postco.core.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class ApiResponseDTO<T> {
     private int status;
-    private String message;
+    private String resultMsg;
     private T result;
 
     @Builder
-    public ApiResponseDTO(final int status, final String message, final T result) {
+    public ApiResponseDTO(final int status, final String resultMsg, final T result) {
         this.status = status;
-        this.message = message;
+        this.resultMsg = resultMsg;
         this.result = result;
     }
 }
