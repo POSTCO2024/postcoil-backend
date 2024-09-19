a9f3014f3bfedbc96e63158fe2acc9f29d5a27ad
Kim, Yerim <yerim343@naver.com>
2024-09-20 02:33:58 +0900

    Feat: websocket stomp 설정

---
+++ websocket-server/src/main/java/com/postco/websocketserver/WebsocketServerApp.java
@@ -0,0 +1,13 @@
+package com.postco.websocketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketServerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketServerApp.class, args);
+	}
+
+}
---
+++ websocket-server/src/test/java/com/postco/websocketserver/WebsocketServerAppTests.java
@@ -0,0 +1,13 @@
+package com.postco.websocketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketServerAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
