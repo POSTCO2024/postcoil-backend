e79d36964f8ddc4855cab7e9d99eb3f269409edb
Young Chae Lee <65876884+dudco0040@users.noreply.github.com>
2024-09-20 18:07:12 +0900

    [FEAT : 작업대상재, 에러재 관리기능 리팩토링
    * Refactoring: 작업대상재, 에러재 관리기능 리팩토링
    
    * Refactoring: 작업대상재, 에러재 관리기능 리팩토링
    
    * Rename: 메서드명 추가

---
+++ control/src/main/java/com/postco/control/service/ErrorMaterialQueryService.java
@@ -0,0 +1,29 @@
+package com.postco.control.service;
+
+import com.postco.control.presentation.dto.TargetViewDTO;
+import com.postco.core.dto.RedisDataContainer;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public interface ErrorMaterialQueryService {
+    /**
+     * 저장된 작업 대상재와 그와 연관된 재료 및 주문을 매핑하는 메서드 (RedisDataContainer 에 매핑)
+     * @return Mono<RedisDataContainer> 작업 대상재, 재료, 주문 정보가 포함된 RedisDataContainer
+     */
+    Mono<RedisDataContainer> mapTargetMaterialsWithRelatedData();
+
+    /**
+     * 저장된 작업 대상재와 그와 연관된 재료 및 주문을 매핑하는 메서드 (TargetViewDTO 에 매핑)
+     * @return Mono<List<TargetViewDTO>> 작업 대상재, 재료, 주문 정보가 매핑된 TargetViewDTO 리스트
+     */
+    Mono<List<TargetViewDTO>> mapToTargetViewDTOs();
+
+    /**
+     * 특정 공정의 에러 상태 작업 대상재를 불러오고 관련 정보를 매핑하는 메서드
+     * @param currProc 현재 공정
+     * @return Mono<List<TargetViewDTO>> 특정 공정의 에러 상태 작업 대상재와 관련 정보가 매핑된 TargetViewDTO 리스트
+     */
+    Mono<List<TargetViewDTO>> getErrorMaterialsByCurrProc(String currProc);
+
+}
