e04cea8622773d9f0657b68ab8cf342050ae596a
Kim, Yerim <yerim343@naver.com>
2024-09-24 01:36:11 +0900

    Fix: 지연로딩 수정

--- operation/src/main/java/com/postco/operation/service/CoilWorkCommandService.java
+++ operation/src/main/java/com/postco/operation/service/CoilWorkCommandService.java
@@ -18,7 +18,9 @@ import reactor.core.scheduler.Schedulers;
 
 import java.time.Duration;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -34,46 +36,68 @@ public class CoilWorkCommandService {
     private final CoilSupplyRepository coilSupplyRepository;
     private final CoilSupplyService coilSupplyService;
     private final MaterialUpdateService materialUpdateService;
-    private final ReactiveRedisTemplate<String, Object> redisTemplate;
+    private final ReactiveRedisTemplate<String, String> redisTemplate;
     private final TransactionTemplate transactionTemplate;
 
     // 작업 지시서에서 보급 요청 시 관련 작업 수행
     // 1) 코일 보급 처리
     // 2) 코일 작업 시작 및 종료
-    @Transactional
     public Mono<Boolean> requestSupply(Long workInstructionId, int supplyCount) {
-        return Mono.fromCallable(() -> transactionTemplate.execute(status -> {
-                    Optional<WorkInstruction> optionalWorkInstruction = workInstructionRepository.findById(workInstructionId);
-                    if (optionalWorkInstruction.isEmpty()) {
-                        throw new IllegalArgumentException("Invalid workInstructionId");
-                    }
-                    WorkInstruction workInstruction = optionalWorkInstruction.get();
+        return Mono.fromCallable(() ->
+                        transactionTemplate.execute(status -> {
+                            WorkInstruction workInstruction = workInstructionRepository.findByIdWithItems(workInstructionId)
+                                    .orElseThrow(() -> new IllegalArgumentException("Invalid workInstructionId"));
 
-                    Optional<CoilSupply> optionalCoilSupply = coilSupplyRepository.findByWorkInstruction(workInstruction);
-                    if (optionalCoilSupply.isEmpty()) {
-                        throw new IllegalArgumentException("No CoilSupply found for the work instruction");
-                    }
-                    return optionalCoilSupply.get();
-                })).subscribeOn(Schedulers.boundedElastic())
-                .flatMap(coilSupply -> {
-                    log.info("작업 지시서 ID: {}, 보급 요청 수량: {}", workInstructionId, supplyCount);
-                    return addItemsToQueue(coilSupply.getWorkInstruction(), supplyCount)
-                            .then(completeSupplyAfterDelay(coilSupply, supplyCount))
+                            CoilSupply coilSupply = coilSupplyRepository.findByWorkInstruction(workInstruction)
+                                    .orElseThrow(() -> new IllegalArgumentException("No CoilSupply found for the work instruction"));
+
+                            log.info("작업 지시서 ID: {}, 보급 요청 수량: {}", workInstructionId, supplyCount);
+
+                            List<WorkInstructionItem> itemsToQueue = workInstruction.getItems().stream()
+                                    .filter(item -> item.getWorkItemStatus() == WorkStatus.PENDING)
+                                    .sorted(Comparator.comparingInt(WorkInstructionItem::getSequence))
+                                    .limit(supplyCount)
+                                    .collect(Collectors.toList());
+
+                            return new Object[]{itemsToQueue, coilSupply};
+                        })
+                ).subscribeOn(Schedulers.boundedElastic())
+                .flatMap(result -> {
+                    List<WorkInstructionItem> itemsToQueue = (List<WorkInstructionItem>) result[0];
+                    CoilSupply coilSupply = (CoilSupply) result[1];
+                    return addItemsToQueue(itemsToQueue)
+                            .doOnSuccess(v -> processSupplyInBackground(coilSupply, itemsToQueue.size()))
                             .thenReturn(true);
                 });
     }
 
     // 작업 아이템을 sequence 순서대로 큐에 추가
     private Mono<Void> addItemsToQueue(WorkInstruction workInstruction, int supplyCount) {
-        return Flux.fromIterable(workInstruction.getItems())
+        List<WorkInstructionItem> pendingItems = workInstruction.getItems().stream()
                 .filter(item -> item.getWorkItemStatus() == WorkStatus.PENDING)
-                .sort(Comparator.comparingInt(WorkInstructionItem::getSequence))
-                .take(supplyCount)
-                .flatMap(item -> redisTemplate.opsForList().leftPush(SUPPLY_QUEUE_KEY, item.getId())
+                .sorted(Comparator.comparingInt(WorkInstructionItem::getSequence))
+                .limit(supplyCount)
+                .collect(Collectors.toList());
+
+        // 보급할 작업 아이템이 없을 경우
+        if (pendingItems.isEmpty()) {
+            return Mono.error(new RuntimeException("더 이상 보급할 작업 아이템이 없습니다."));
+        }
+
+        return Flux.fromIterable(pendingItems)
+                .flatMap(item -> redisTemplate.opsForList().leftPush(SUPPLY_QUEUE_KEY, item.getId().toString())
                         .doOnSuccess(result -> log.info("[보급 요청 성공] 큐에 작업 아이템 ID : {} 추가됨", item.getId()))
                         .doOnError(error -> log.error("[보급 요청 실패] 큐 추가 중 오류 발생: {}", error.getMessage())))
                 .then(Mono.fromRunnable(() ->
-                        log.info("[보급 요청 완료] 총 {}개의 작업 아이템이 큐에 추가됨. 작업 지시서 ID: {}", supplyCount, workInstruction.getId())));
+                        log.info("[보급 요청 완료] 총 {}개의 작업 아이템이 큐에 추가됨. 작업 지시서 ID: {}", pendingItems.size(), workInstruction.getId())));
+    }
+
+    private void processSupplyInBackground(CoilSupply coilSupply, int supplyCount) {
+        Mono.delay(Duration.ofSeconds(DELAY_IN_SECONDS))
+                .then(completeSupplyAfterDelay(coilSupply, supplyCount))
+                .doOnSuccess(v -> log.info("백그라운드 보급 처리 완료"))
+                .doOnError(error -> log.error("백그라운드 보급 처리 중 오류 발생", error))
+                .subscribe();
     }
 
     // 일정 시간 후 보급 완료 처리 및 큐에서 아이템 작업 시작
@@ -90,7 +114,7 @@ public class CoilWorkCommandService {
     private Mono<Void> processQueuedItems(int count) {
         return Flux.range(0, count)
                 .flatMap(i -> redisTemplate.opsForList().rightPop(SUPPLY_QUEUE_KEY)
-                        .cast(Long.class)
+                        .flatMap(idString -> Mono.justOrEmpty(Long.parseLong(idString))) // String to Long 변환
                         .flatMap(this::startWorkOnItem)
                         .doOnNext(id -> log.info("큐에서 작업 아이템 ID 처리: {}", id))
                         .switchIfEmpty(Mono.fromRunnable(() -> log.info("더 이상 처리할 아이템이 없습니다."))))
@@ -115,8 +139,8 @@ public class CoilWorkCommandService {
     private Mono<Void> scheduleWorkCompletion(WorkInstructionItem item) {
         long expectedDurationMinutes = item.getExpectedItemDuration();
         return completeWorkAfterDuration(item.getId(), item.getWorkInstruction().getId(), expectedDurationMinutes)
-                .doOnSubscribe(s -> log.info("작업 완료 프로세스가 시작되었습니다 - 아이템 ID: {}", item.getId()))
-                .doOnError(error -> log.error("작업 완료 프로세스 시작 실패 - 아이템 ID: {}, 에러: {}", item.getId(), error.getMessage()));
+                .doOnSubscribe(s -> log.info("[작업 완료] 완료 프로세스 start... - 아이템 ID: {}", item.getId()))
+                .doOnError(error -> log.error("[작업 완료] 완료 프로세스 에러 - 아이템 ID: {}, 에러: {}", item.getId(), error.getMessage()));
     }
 
     // step 3.
