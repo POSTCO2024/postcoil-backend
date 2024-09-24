1aefae9fe7173512ac5cbd30b277152b38eebb4e
hanriverkitty <danieltls@naver.com>
2024-09-24 15:13:06 +0900

    Feat: 웹소켓 연결완료 및 여러상화황에 맞는 메서드 생성

--- websocket/src/main/java/com/postco/websocket/service/CoilService.java
+++ websocket/src/main/java/com/postco/websocket/service/CoilService.java
@@ -3,18 +3,39 @@ package com.postco.websocket.service;
 
 import com.postco.websocket.web.CoilData;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class CoilService {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public CoilService(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+
     public CoilData processCoilData(String message) {
-        log.info("메세지 요청 수신 완료 : ", message);
+        log.info("메세지 요청 수신 완료 : {}", message);
         return CoilData.builder()
                 .materialId(1L)
                 .width(7000.0)
                 .thickness(0.12)
                 .nextCurr("1CAL")
                 .build();
+    }
+
+    // 로직에서 바로 메시지 전송, 특정한 주체한테 못넘김
+    public void directMessageToClient(String message) {
+        messagingTemplate.convertAndSend("/topic/coilData", "directMessage");
+    }
+
+    // 정해진 시간마다 메시지 전송,
+    @Scheduled(fixedRate = 3000)
+    public void sendScheduleMessage() {
+        log.info("scheduled Messaging");
+        messagingTemplate.convertAndSend("/topic/coilData", "schduleMessage");
     }
 }
