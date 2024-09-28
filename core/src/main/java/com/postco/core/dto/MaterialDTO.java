f1411d515f61f459b2b412e682c683d26bc912e2
hanriverkitty <danieltls@naver.com>
2024-09-28 17:44:37 +0900

    Fix: rebase 에러 해결

--- core/src/main/java/com/postco/core/dto/MaterialDTO.java
+++ core/src/main/java/com/postco/core/dto/MaterialDTO.java
@@ -70,36 +70,4 @@ public class MaterialDTO {
         private String coilTypeCode;
         private Long orderId;
     }
-
-    @Data
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @JsonIgnoreProperties(ignoreUnknown = true) // error 수정 - maxbort 2024-09-19
-    public static class Message implements DTO {
-        private Long materialId;
-        private String materialNo;
-        private String status;
-        private String factoryCode;
-        private String opCode;
-        private String currProc;
-        private String materialType;
-        private String progress;
-        private double outerDia;
-        private double innerDia;
-        private double width;
-        private double thickness;
-        private double length;
-        private double weight;
-        private double temperature;
-        private double totalWeight;
-        private String passProc;
-        private String remProc;
-        private String preProc;
-        private String nextProc;
-        private String storageLoc;
-        private String yard;
-        private String coilTypeCode;
-        private Long orderId;
-    }
 }
