a5d532a331f77de314727c7dd8b3c0c9a5a34cca
hanriverkitty <danieltls@naver.com>
2024-09-29 21:34:05 +0900

    Feat: 완료된ê설정한 기간내에 있는 작업지시서 조회

--- operation/src/main/java/com/postco/operation/service/WorkInstructionService.java
+++ operation/src/main/java/com/postco/operation/service/WorkInstructionService.java
@@ -40,5 +40,5 @@ public interface WorkInstructionService {
     /**
      * 끝나지 않은 작업지시서 조회
      */
-    public Mono<List<WorkInstructionDTO.View>> getCompletedWorkInstructions(String process);
+    public Mono<List<WorkInstructionDTO.View>> getCompletedWorkInstructions(String process, String startDate, String endDate);
 }
