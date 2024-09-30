2b3ca7e9bd9319e82e9c90237db7362ab6331b8c
hanriverkitty <danieltls@naver.com>
2024-10-01 00:32:13 +0900

    Fix: rebase 문 ãmain 과 비교하여 수정

--- operation/src/main/java/com/postco/operation/presentation/MaterialController.java
+++ operation/src/main/java/com/postco/operation/presentation/MaterialController.java
@@ -1,21 +1,17 @@
 package com.postco.operation.presentation;
 
 import com.postco.operation.service.KafkaMessageService;
-import com.postco.operation.service.impl.WebSocketServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/materials")
-@CrossOrigin(origins = "http://localhost:6050")
 @RequiredArgsConstructor
 public class MaterialController {
     private final KafkaMessageService kafkaMessageService;
-    private final WebSocketServiceImpl webSocketService;
 
     @GetMapping("/send")
     public ResponseEntity<String> getMaterials() {
@@ -28,10 +24,4 @@ public class MaterialController {
         kafkaMessageService.sendOrders();
         return ResponseEntity.ok("All orders sent successfully");
     }
-
-    @GetMapping("/websocketTest")
-    public void getMaterialAndSendToWebSocket() {
-        webSocketService.findMaterials();
-    }
-
 }
--- operation/src/main/java/com/postco/operation/service/WebSocketService.java
+++
@@ -1,9 +0,0 @@
-package com.postco.operation.service;
-
-import com.postco.operation.domain.entity.Materials;
-
-import java.util.List;
-
-public interface WebSocketService {
-    List<Materials> findMaterials();
-}
