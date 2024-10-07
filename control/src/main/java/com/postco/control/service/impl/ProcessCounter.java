f561706b4255c76b394e4a1d09b2d3c13263816d
dudco0040 <dudco0040@gmail.com>
2024-10-07 23:25:30 +0900

    Refactoring: switch 문 수정

--- control/src/main/java/com/postco/control/service/impl/ProcessCounter.java
+++ control/src/main/java/com/postco/control/service/impl/ProcessCounter.java
@@ -1,40 +1,36 @@
 package com.postco.control.service.impl;
 
 import com.postco.control.presentation.dto.response.Fc001aDTO;
-import com.postco.core.dto.MaterialDTO;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
+import java.util.function.Consumer;
 
 @Slf4j
 public class ProcessCounter {
+    // 각 차공정에 따른 필드 업데이트 로직을 Map에 미리 정의
+    private static final Map<String, Consumer<Fc001aDTO.Table>> processCountUpdater = Map.of(
+            "1CAL", table -> table.setProc1CAL(table.getProc1CAL() + 1),
+            "2CAL", table -> table.setProc2CAL(table.getProc2CAL() + 1),
+            "1EGL", table -> table.setProc1EGL(table.getProc1EGL() + 1),
+            "2EGL", table -> table.setProc2EGL(table.getProc2EGL() + 1),
+            "1CGL", table -> table.setProc1CGL(table.getProc1CGL() + 1),
+            "2CGL", table -> table.setProc2CGL(table.getProc2CGL() + 1),
+            "101", table -> table.setProc1Packing(table.getProc1Packing() + 1),
+            "201", table -> table.setProc2Packing(table.getProc2Packing() + 1)
+    );
+
+    // 차공정에 따른 카운팅을 수행하는 메서드
     public static void countNextProc(Fc001aDTO.Table tableEntry, String nextProc) {
-        switch (nextProc) {
-            case "1CAL":
-                tableEntry.setProc1CAL(tableEntry.getProc1CAL() + 1);
-                break;
-            case "2CAL":
-                tableEntry.setProc2CAL(tableEntry.getProc2CAL() + 1);
-                break;
-            case "1EGL":
-                tableEntry.setProc1EGL(tableEntry.getProc1EGL() + 1);
-                break;
-            case "2EGL":
-                tableEntry.setProc2EGL(tableEntry.getProc2EGL() + 1);
-                break;
-            case "2CGL":
-                tableEntry.setProc2EGL(tableEntry.getProc2CGL() + 1);
-                break;
-            case "101":
-                tableEntry.setProc1Packing(tableEntry.getProc1Packing() + 1);
-                break;
-            case "201":
-                tableEntry.setProc2Packing(tableEntry.getProc2Packing() + 1);
-                break;
-            // 다른 차공정이 추가되면 여기에 계속 추가
-            default:
-                log.warn("알 수 없는 차공정: " + nextProc);
-                break;
+        // 해당 차공정에 대한 업데이트 로직이 있는지 확인
+        Consumer<Fc001aDTO.Table> updater = processCountUpdater.get(nextProc);
+
+        if (updater != null) {
+            // 해당 공정이 존재하면 업데이트 수행
+            updater.accept(tableEntry);
+        } else {
+            // 없는 공정일 경우 경고 로그 출력
+            log.warn("알 수 없는 차공정: " + nextProc);
         }
     }
 }
